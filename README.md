db508443ffa1ed39d89240be855667e8eb635fd3
Sam Wahbeh <stwahbeh@gmail.com>
2015-11-09 19:39:36 -0800

    updating files

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # spotify
 
-Ready Me
+1. The purpose of this app is to allow users to guess which playlist cover belongs to which playlist. Some people name their playlists really weird and I thought it would be interesting to see if a person could guess the name.
+
+2. http://students.washington.edu/stwahbeh/info343/spotify
+
+3. I talked to Seung a little bit about some of the spotify api.  And over camp I talked to Arvind.
+
+4.it took me about 20 hours to complete the challenge.
+
+
--- js/spotify.js
+++ js/spotify.js
@@ -1,15 +1,39 @@
+//2ca5d9af099947f18a90d7b1effa0c84 spotify Client ID  
+//ab2a2536b6524864bbe2071afdf8caa9 spotify secret Client ID
 var data;
-var baseUrl = 'https://api.spotify.com/v1/search?type=track&query='
+var catPlaylistUrl = 'https://api.spotify.com/v1/browse/categories/'
+var categoriesUrl = 'https://api.spotify.com/v1/browse/categories/{category_id}'
 var myApp = angular.module('myApp', [])
 
+
+//gets featured categories on load
 var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
-  $scope.audioObject = {}
-  $scope.getSongs = function() {
-    $http.get(baseUrl + $scope.track).success(function(response){
-      data = $scope.tracks = response.tracks.items
-      
+  
+  $scope.imageObject = {}
+  $scope.getCategories = function() {
+    $http.get(categoriesUrl).success(function(response){
+      data = $scope.categories = response.categories.images 
+      console.log(data);     
     })
+
   }
+  $scope.getCategories();
+
+
+
+//{category_id}/playlists
+//   $scope.getPlaylists = function(category){
+//     $('#display').empty()
+//     $scope.playlistObject = {}
+//     $http.get(catPlaylistUrl + $scope.genre).success(function(response){
+//       data = $scope.playlists = response.genre.images
+
+//     })
+//   }
+
+
+
+  //plays songs
   $scope.play = function(song) {
     if($scope.currentSong == song) {
       $scope.audioObject.pause()
@@ -24,6 +48,7 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
     }
   }
 })
+
 
 // Add tool tips to anything with a title property
 $('body').tooltip({
